f01efce80249648d01459a0d1e1ae318632a2396
ngophuc29 <ngophuc2911@gmail.com>
2025-02-14 08:15:14 +0700

    2. chuyen name va message vo mot obj,sau do phan chia tin nhan ngGui va ngNhan sang ben trai va ben phai, va rang buoc cac truong hop tNhan qua dai , them su kien enter vao nut gui

--- public/js/client.js
+++ public/js/client.js
@@ -7,25 +7,50 @@ const btn_send = document.getElementById("btn_send")
 const ul_message=document.getElementById("ul_message")
 const socket = io.connect()
 
+
+let myname=""
+
 socket.on("connect", (data) => {
     console.log(data)
 })
 btn_join.addEventListener("click", () => {
-    const name = inputname.value
+    myname = inputname.value
     const room = inputroom.value
     socket.emit("join", room)
+    alert("Join room " + room)
 })
 
-btn_send.addEventListener("click", () => { 
+const sendMessage = () => {
     const message = inputmessage.value
-    const name = inputname.value
+    if (message === "") {
+        return
+    }
 
-    socket.emit("message", name+" : "+ message)
+    const obj = {
+        name: myname,
+        message: message
+    }
+
+    socket.emit("message", JSON.stringify(obj))
+    inputmessage.value = ""
+    inputmessage.focus()
+}
+btn_send.addEventListener("click", sendMessage)
+inputmessage.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        sendMessage()
+    }
 })
-
 socket.on("thread", (data) => { 
+
+    const obj=JSON.parse(data)
     console.log(data)
     const li = document.createElement("li")
-    li.innerHTML = data
+    li.innerHTML = obj.message
+
+    if (obj.name === myname) { 
+        li.classList.add("right")
+    }
+
     ul_message.appendChild(li)
 })
