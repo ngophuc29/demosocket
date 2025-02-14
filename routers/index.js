c847040366c7ada65dc246d02dfd47f1694f66f2
ngophuc29 <ngophuc2911@gmail.com>
2025-02-14 09:55:54 +0700

    3. tao UI va viet ham de fetch Login

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,12 @@
 
 const accountRouter = require('./account.router');
 module.exports = (app) => {
+    app.use("/", (req, res) => {
+        return res.render('login.ejs')
+    })
+
     app.use('/api/accounts', accountRouter)
+
     app.use('/chat', (req, res) => {
         return res.render('chat.ejs')
     })
